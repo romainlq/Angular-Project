114e7faa76e692500438eeff1b8af86dbf423737
romainlequellec <romain.lequellec@epfedu.fr>
2016-03-15 23:29:38 +0100

    Angular Fonctionnel

--- js/app.js
+++ js/app.js
@@ -16,10 +16,8 @@
     */
     $scope.loading=false; // Charge les données ou non
 
-    var option=[
-      ville='null'; //ville recherchée
-      recherche="null"; //choix hébergement, activité, poi, etc ...
-    ];
+    recherche="null"; //choix hébergement, activité, poi, etc ...
+   
   }]);
 
   //Controller formulaire
@@ -37,44 +35,18 @@
 
 
       function initMap(){
-         // Options de la Google map
-         var mapOptions = {
-
-
       // Options de la Google map
       var mapOptions = {
           center: {
               lat:48.85703523304221,
               lng:2.3490142822265625
           },
-
            zoom:12
       };
 
       $scope.map = new google.maps.Map(document.getElementById("map"),mapOptions);
       }
 
-      };
-      // Création de la map
-      $scope.map = new google.maps.Map(document.getElementById("map"),mapOptions);
-
+    
   }]);
-
-
-  //Controller du lieu choisi
-  app.controller('placeController', ['$http',function($http){
-    place = this;
-    place.donnees = [];
-
-    $http.get('http://tour-pedia.org/api/getPlaces?location=' + ville).success(function(data){
-       place.donnees = data;
-       });
-  }]);
-
-  //Controller review renvoyée par l'API
-  app.controller('reviewController', function(){
-
-  });
-
-
 })();
